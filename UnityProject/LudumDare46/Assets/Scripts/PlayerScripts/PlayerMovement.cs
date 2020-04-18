d651b8e844ded14540ddbbd8b07a98a0e4e2dff2
Griffin Gaskins <21.griffin.gaskins@gmail.com>
2020-04-18 15:53:37 -0700

    added player movement

---
+++ UnityProject/LudumDare46/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public Animator controller;
+
+    
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        if(movement.x == 0 && movement.y == 1)
+        {
+            controller.SetTrigger("Up");
+        }
+        else if (movement.x == 0 && movement.y == -1)
+        {
+            controller.SetTrigger("Down");
+        }
+        else if (movement.x == 1 && movement.y == 0)
+        {
+            controller.SetTrigger("Right");
+        }
+        else if (movement.x == -1 && movement.y == 0)
+        {
+            controller.SetTrigger("Left");
+        }
+        else if(movement.x == 0 && movement.y == 0)
+        {
+            controller.SetTrigger("Idle");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ UnityProject/LudumDare46/Assets/Scripts/PlayerScripts/TiledPlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiledPlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Transform movePoint;
+    public Animator anim;
+
+    public LayerMask stoppers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            if (Input.GetAxisRaw("Horizontal") == -1)
+            {
+                anim.SetTrigger("Left");
+            }
+            else if (Input.GetAxisRaw("Horizontal") == 1)
+            {
+                anim.SetTrigger("Right");
+            }
+            else if(Input.GetAxisRaw("Vertical") == 1)
+            {
+                anim.SetTrigger("Up");
+            }
+            else if(Input.GetAxisRaw("Vertical") == -1)
+            {
+                anim.SetTrigger("Down");
+            }
+        }
+        else
+        {
+            anim.SetTrigger("Idle");
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= .085f)
+        {
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .4f, stoppers))
+                {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+            }
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .4f, stoppers))
+                {
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
+            }
+        }
+        
+    }
+}
